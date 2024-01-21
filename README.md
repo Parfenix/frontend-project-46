a720bda6fe28b35fc474ae6c0c0dc4e124bf63c0
Denis Parfenov <Parfenix@gmail.com>
2024-01-21 16:14:26 +0300

    Implemented a search for differences between two flat json files. Added an askinema to the readme with an example of how the package works

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Parfenix/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Parfenix/frontend-project-46/actions)
+
+# Вычислитель отличий (JavaScript)
+
+Вычислитель отличий – программа, определяющая разницу между двумя структурами данных. Подобный механизм используется при выводе тестов или при автоматическом отслеживании изменении в конфигурационных файлах.
+
+#### Возможности утилиты:
+
+- Поддержка разных входных форматов: yaml, json
+- Генерация отчета в виде plain text, stylish и json
+
+#### Пример работы программы
+
+[![asciicast](https://asciinema.org/a/XnKMjD9sdWlA3LRiHb5qrEVE6.svg)](https://asciinema.org/a/XnKMjD9sdWlA3LRiHb5qrEVE6)
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,52 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
 
 const program = new Command();
+
+const readFile = (filePath) => {
+  const currentDirecory = process.cwd();
+  const fullPath = path.resolve(currentDirecory, filePath);
+  return fs.readFileSync(fullPath, { encoding: 'utf8' });
+};
 
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format')
-  .argument('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const file1 = readFile(filepath1);
+    const file2 = readFile(filepath2);
+
+    const parseFile1 = JSON.parse(file1);
+    const parseFile2 = JSON.parse(file2);
+
+    const keys1 = Object.keys(parseFile1);
+    const keys2 = Object.keys(parseFile2);
+
+    const shareKeys = _.union(keys1, keys2).sort();
+
+    const genDiff = (key) => {
+      let string = '';
+      if (parseFile1[key] === parseFile2[key]) {
+        string = `  ${key}: ${parseFile1[key]}\n`;
+      } if (parseFile1[key] !== parseFile2[key]) {
+        string = `- ${key}: ${parseFile1[key]}\n + ${key}: ${parseFile2[key]}\n`;
+      } if (!_.has(parseFile1, key)) {
+        string = `+ ${key}: ${parseFile2[key]}\n`;
+      } if (!_.has(parseFile2, key)) {
+        string = `- ${key}: ${parseFile1[key]}\n`;
+      }
+      return string;
+    };
+
+    const result = shareKeys.map((key) => genDiff(key)).join(' ');
+    console.log(`{\n ${result}}`);
+  });
 
 program.parse(process.argv);
